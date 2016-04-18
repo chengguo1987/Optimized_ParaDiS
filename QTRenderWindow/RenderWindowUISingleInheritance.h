89588a26e1acffc80c2ccc4d2eb332192f251554
chengguo1987 <chengguo1987@gmail.com>
2016-04-18 10:12:58 -0500

    Updated the header in accordance with the source file

--- QTRenderWindow/RenderWindowUISingleInheritance.h
+++ QTRenderWindow/RenderWindowUISingleInheritance.h
@@ -4,6 +4,9 @@
 #include <vtkSmartPointer.h>
 #include <vtkPLYReader.h>
 #include <vtkCubeSource.h>
+#include <vtkInteractorStyleTrackballActor.h>
+#include <vtkObjectFactory.h>
+//#include <vtkPolyData.h>
 
 #include <QMainWindow>
 #include <QFileDialog> 
@@ -11,6 +14,18 @@
 
 // Forward Qt class declarations
 class Ui_RenderWindowUISingleInheritance;
+
+class MouseInteractorStyle6 : public vtkInteractorStyleTrackballActor
+{
+public:
+  vtkPolyData* Sphere;
+  vtkActor* CubeActor;
+  static MouseInteractorStyle6* New();
+  vtkTypeMacro(MouseInteractorStyle6, vtkInteractorStyleTrackballActor);
+  virtual void OnLeftButtonDown();
+  virtual void OnMiddleButtonUp();
+  virtual void OnRightButtonDown();
+};
  
 class RenderWindowUISingleInheritance : public QMainWindow
 {
@@ -27,7 +42,7 @@ public slots:
   virtual void slotExit();
   virtual void slotOpen();
   virtual void slotUpdate();
-
+  virtual void slotReset();
 private:
  
   // Designer form
